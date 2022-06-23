17eb934a83e07a8827a56244b5cfd995fe78f662
Ilmir <Ilmir131313@yandex.ru>
2022-06-23 19:31:36 +0300

    Khafizov_Ilmir_dz_Lesson10

---
+++ Khafizov_Ilmir_dz_3.py
@@ -0,0 +1,21 @@
+class Cell:
+    def __init__(self, nums):
+        self.nums = nums
+    def make_order(self, rows):
+        return '\n'.join(['*' * rows for _ in range(self.nums // rows)]) + '\n' + '*' * (self.nums % rows)
+    def __str__(self):
+        return str(self.nums)
+    def __add__(self, other):
+        return Cell(self.nums + other.nums)
+    def __sub__(self, other):
+        return self.nums - other.nums if self.nums - other.nums > 0 \
+        else 'Ячеек в первой клетке меньше или равно второй, вычитание не возможно'
+    def __mul__(self, other):
+        return 'Multiply of cells is ' + str(self.nums * other.nums)
+    def __truediv__(self, other):
+        return 'Truediv of cells is ' + str(self.nums / other.nums)
+cell_1 = Cell(100)
+cell_2 = Cell(20)
+print(cell_2)
+print(cell_2 + cell_1)
+print(cell_1.make_order(12))
